90a66d0aac50f8b93681f76c055584de5324efb5
seinflaw <horizontal.genki3317@gmail.com>
2016-08-03 21:09:54 +0900

    add ChatControllerTest

---
+++ src/test/java/com/k4t2/controller/ChatControllerTest.java
@@ -0,0 +1,27 @@
+package com.k4t2.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by seinflaw on 2016/08/03.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class ChatControllerTest {
+
+    @Test
+    public void talk() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void talk1() throws Exception {
+        fail();
+    }
+
+}
