503fbe883dc823efaa3ba37cf7d85212cb8902a6
Ken.Kaiho <ken-kaiho@bm-sms.co.jp>
2016-08-03 20:25:23 +0900

    単体テストで追加

--- src/main/java/com/k4t2/controller/AuthController.java
+++ src/main/java/com/k4t2/controller/AuthController.java
@@ -1,15 +1,29 @@
 package com.k4t2.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.k4t2.model.AuthResult;
+
+/**
+ * 認証を行うControllerです。
+ * ユーザーそのものの作成はUserControllerで行われます。
+ * 
+ * @author ken-kaiho
+ */
 @RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
 public class AuthController {
 
-	@RequestMapping(method=RequestMethod.POST)
-	public void login() {
+	@RequestMapping(method=RequestMethod.POST,path = "/{userId}")
+	public AuthResult login(@RequestBody String password) {
 		
+		
+		return new AuthResult("OK");
 	}
 	
 	@RequestMapping(method=RequestMethod.DELETE)
