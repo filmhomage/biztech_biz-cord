d711fcc4da16a60e78959251319c4ac6add91d95
Jonghyun Kim <filmhomage@hotmail.com>
2016-08-24 21:12:05 +0900

    Add to room model, controller

---
+++ src/main/java/com/k4t2/repository/doma/RoomDao.java
@@ -0,0 +1,18 @@
+package com.k4t2.repository.doma;
+import java.util.List;
+import org.seasar.doma.Dao;
+//import org.seasar.doma.Insert;
+import org.seasar.doma.Select;
+import org.seasar.doma.boot.ConfigAutowireable;
+
+import com.k4t2.model.Room;
+
+@Dao
+@ConfigAutowireable
+public interface RoomDao {
+    @Select
+    public List<Room> selectRoom();
+//    @Insert
+//    public int insert(Room user);
+}
+
